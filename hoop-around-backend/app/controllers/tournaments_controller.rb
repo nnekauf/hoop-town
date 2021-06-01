98fd9c8616318237692b1c0caf0c6124490288fb
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-01 03:05:42 -0400

    adds CRUD to tournament controller actions

--- hoop-around-backend/app/controllers/tournaments_controller.rb
+++ hoop-around-backend/app/controllers/tournaments_controller.rb
@@ -1,2 +1,31 @@
 class TournamentsController < ApplicationController
+    def index
+        render json: Tournament.all #, key_transform: :camel_lower
+    end
+
+    def show
+       
+        tournament = Tournament.find_by(id: params[:id])
+        render json: tournament
+    end
+
+    def creatt
+       
+        tournament = Tournament.create(tournament_params)
+        render json: tournament
+    end
+
+    def destroy
+        tournament = tournament.find_by(id: params[:id])
+        
+        tournament.destroy
+        render json: {message: "success"}
+    end
+
+    private
+
+    def tournament_params
+
+        params.require(:tournament). permit(:name)
+    end
 end
