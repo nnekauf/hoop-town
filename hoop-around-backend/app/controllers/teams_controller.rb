2df6fa06c4ca3d2da40501267d04b119d05b8db8
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-01 03:04:32 -0400

    adds CRUD to team controller actions

--- hoop-around-backend/app/controllers/teams_controller.rb
+++ hoop-around-backend/app/controllers/teams_controller.rb
@@ -1,2 +1,31 @@
 class TeamsController < ApplicationController
+    def index
+        render json: Team.all #, key_transform: :camel_lower
+    end
+
+    def show
+       
+        team = Team.find_by(id: params[:id])
+        render json: team
+    end
+
+    def create
+       
+        team = Team.create(team_params)
+        render json: team
+    end
+
+    def destroy
+        team = Team.find_by(id: params[:id])
+        
+        Team.destroy
+        render json: {message: "success"}
+    end
+
+    private
+
+    def team_params
+
+        params.require(:team). permit(:name)
+    end
 end
