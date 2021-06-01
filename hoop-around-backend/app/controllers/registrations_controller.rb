e92519435e446fa8a4769ee8766428cbd5c44281
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-01 03:03:28 -0400

    adds CRUD to registration controller actions

--- hoop-around-backend/app/controllers/registrations_controller.rb
+++ hoop-around-backend/app/controllers/registrations_controller.rb
@@ -1,2 +1,31 @@
 class RegistrationsController < ApplicationController
+    def index
+        render json: Registration.all #, key_transform: :camel_lower
+    end
+
+    def show
+       
+        registration = Registration.find_by(id: params[:id])
+        render json: registration
+    end
+
+    def create
+       
+        registration = Registration.create(registration_params)
+        render json: registration
+    end
+
+    def destroy
+        registration = Registration.find_by(id: params[:id])
+        
+        registration.destroy
+        render json: {message: "success"}
+    end
+
+    private
+
+    def registration_params
+
+        params.require(:registration). permit(:name)
+    end
 end
