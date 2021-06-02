81abc5b54ef6d86720242212ec93e38c2da48f24
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-02 15:14:37 -0400

    namespacing routes

--- hoop-around-backend/config/routes.rb
+++ hoop-around-backend/config/routes.rb
@@ -1,18 +1,24 @@
 Rails.application.routes.draw do
-  resources :registrations
-  resources :tournaments
-  resources :teams
-  resources :players
-  resources :coaches
-  resources :lists
 
-  get '/signup', to: "coaches#new", as: "signup" 
-    post '/signup', to: "coaches#create"
+  namespace :api do
+    namespace :v1 do
+      resources :registrations
+      resources :tournaments
+      resources :teams
+      resources :players
+      resources :coaches
+      resources :lists
+    
+      get '/signup', to: "coaches#new", as: "signup" 
+        post '/signup', to: "coaches#create"
+    
+        get '/login', to: "sessions#new", as: "login" 
+        post '/login', to: "sessions#create"
+        post '/logout', to: "sessions#destroy" 
+    
+        get "/auth/facebook/callback", to: "sessions#create_with_fb"
+    end
+  end
 
-    get '/login', to: "sessions#new", as: "login" 
-    post '/login', to: "sessions#create"
-    post '/logout', to: "sessions#destroy" 
-
-    get "/auth/facebook/callback", to: "sessions#create_with_fb" 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   
 end
