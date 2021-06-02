aa167d266e25c32f17858f6d8de93d2088eac391
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-02 13:53:45 -0400

    adds associations between tournaments teams and registrations

--- hoop-around-backend/app/models/team.rb
+++ hoop-around-backend/app/models/team.rb
@@ -1,4 +1,6 @@
 class Team < ApplicationRecord
   belongs_to :coach
   has_many :players 
+  has_many tournaments, through: :registrations 
+  has_many :registrations
 end
--- hoop-around-backend/app/models/tournament.rb
+++ hoop-around-backend/app/models/tournament.rb
@@ -1,2 +1,4 @@
 class Tournament < ApplicationRecord
+    has_many teams, through: :registrations 
+    has_many :registrations 
 end
