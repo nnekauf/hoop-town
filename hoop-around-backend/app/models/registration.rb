e3493282f00d698c366b78c0b4a38cc7092d5754
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-03 04:04:05 -0400

    adds validations

--- hoop-around-backend/app/models/registration.rb
+++ hoop-around-backend/app/models/registration.rb
@@ -2,6 +2,5 @@ class Registration < ApplicationRecord
   belongs_to :team
   belongs_to :tournament
 
-  validates_presence_of :team_id
-  validates_presence_of :tournament_id
+
 end
--- hoop-around-backend/app/models/user.rb
+++ hoop-around-backend/app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ApplicationRecord
-    belongs_to :team
     enum role: [:coach, :player]
+
+    belongs_to :team
+  
+    has_secure_password 
+
+    validates :username, uniqueness: true
+    validates :email, uniqueness: true
+    validates :contact_number, uniqueness: true
+    validates_length_of :contact_number, is: 10,  message: "Number must be 10 digits long"
+  
+    validates_presence_of :bio
+    validates_presence_of :first_name
+    validates_presence_of :last_name
+    validates_presence_of :role
 end
