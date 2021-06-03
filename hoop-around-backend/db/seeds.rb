615f72291ee1af0821320aebfca44650ac6bc0f6
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-03 05:30:20 -0400

    adds two more players to the database

--- hoop-around-backend/db/seeds.rb
+++ hoop-around-backend/db/seeds.rb
@@ -7,16 +7,18 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-User.create(email: "thomas@bkballers.com", username: "thomas" , password: "password", bio: "The best bball Player there is", first_name: "Kyle", last_name: "Thomas", contact_number: 9998880978, team_id: 1, role: 0)
-User.create(email: "mekel@hoopers.com", username: "mekelcoach" , password: "password", bio: "Been coaching since 5", first_name: "Mekel", last_name:"B", contact_number: 4448880978, team_id: 2, role: 0)
+# User.create(email: "thomas@bkballers.com", username: "thomas" , password: "password", bio: "The best bball Player there is", first_name: "Kyle", last_name: "Thomas", contact_number: 9998880978, team_id: 1, role: 0)
+# User.create(email: "mekel@hoopers.com", username: "mekelcoach" , password: "password", bio: "Been coaching since 5", first_name: "Mekel", last_name:"B", contact_number: 4448880978, team_id: 2, role: 0)
 
-User.create(email: "johnny@bkballers.com", username: "johnny" , password: "password", bio: "Been balling since '98", first_name: "Johnny", last_name:"Johnson", contact_number: 9997770978, team_id: 1, role: 1)
-User.create(email: "tyler@hoopers.com", username: "tyler" , password: "password", bio: "Been hooping since '98", first_name: "Tyler", last_name:"Johnson", contact_number: 4447770978, team_id: 2, role: 1)
+# User.create(email: "johnny@bkballers.com", username: "johnny" , password: "password", bio: "Been balling since '98", first_name: "Johnny", last_name:"Johnson", contact_number: 9997770978, team_id: 1, role: 1)
+# User.create(email: "tyler@hoopers.com", username: "tyler" , password: "password", bio: "Been hooping since '98", first_name: "Tyler", last_name:"Johnson", contact_number: 4447770978, team_id: 2, role: 1)
+User.create(email: "henry@bkballers.com", username: "henry" , password: "password", bio: "best to ever do it", first_name: "Henry", last_name:"Johnson", contact_number: 3457770978, team_id: 1, role: 1)
+User.create(email: "sam@hoopers.com", username: "sam" , password: "password", bio: "I let my actions do the talking", first_name: "Sam", last_name:"Johnson", contact_number: 5437770978, team_id: 2, role: 1)
 
-Team.create(name: "Brooklyn Ballers")
-Team.create(name: "Hoopers")
+# Team.create(name: "Brooklyn Ballers")
+# Team.create(name: "Hoopers")
 
-Tournament.create(name: "Hoop Stars", venue: "BKG Gym", street: "231 Ballhill Road", city: "New York", state: "NY", zipcode: 11234, date_time: DateTime.parse("17/07/2021 11:00"), host: "Brooklyn Ballers", about: "Lets see who's the best in Brooklyn")
+# Tournament.create(name: "Hoop Stars", venue: "BKG Gym", street: "231 Ballhill Road", city: "New York", state: "NY", zipcode: 11234, date_time: DateTime.parse("17/07/2021 11:00"), host: "Brooklyn Ballers", about: "Lets see who's the best in Brooklyn")
 
-Registration.create(team_id: 1, tournament_id: 1)
-Registration.create(team_id: 2, tournament_id: 1)
+# Registration.create(team_id: 1, tournament_id: 1)
+# Registration.create(team_id: 2, tournament_id: 1)
