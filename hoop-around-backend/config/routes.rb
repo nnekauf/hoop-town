310e1466cc87baad085111dda740c1f74245f8e7
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-03 02:16:15 -0400

    comments out signup/login routes

--- hoop-around-backend/config/routes.rb
+++ hoop-around-backend/config/routes.rb
@@ -9,14 +9,14 @@ Rails.application.routes.draw do
       resources :coaches
       resources :lists
     
-      get '/signup', to: "coaches#new", as: "signup" 
-        post '/signup', to: "coaches#create"
+      # get '/signup', to: "coaches#new", as: "signup" 
+      #   post '/signup', to: "coaches#create"
     
-        get '/login', to: "sessions#new", as: "login" 
-        post '/login', to: "sessions#create"
-        post '/logout', to: "sessions#destroy" 
+      #   get '/login', to: "sessions#new", as: "login" 
+      #   post '/login', to: "sessions#create"
+      #   post '/logout', to: "sessions#destroy" 
     
-        get "/auth/facebook/callback", to: "sessions#create_with_fb"
+      #   get "/auth/facebook/callback", to: "sessions#create_with_fb"
     end
   end
 
