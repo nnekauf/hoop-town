88a652d0be4c2ec7127c946d23a50222a048bd10
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-03 04:10:40 -0400

    adds team_id column to users table

--- hoop-around-backend/db/schema.rb
+++ hoop-around-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_03_064518) do
+ActiveRecord::Schema.define(version: 2021_06_03_080926) do
 
   create_table "registrations", force: :cascade do |t|
     t.integer "team_id", null: false
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2021_06_03_064518) do
     t.integer "role"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "team_id"
   end
 
   add_foreign_key "registrations", "teams"
