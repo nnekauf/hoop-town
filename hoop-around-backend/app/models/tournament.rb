6b1dbd9fd85586a5067b9af8198641335bd1a453
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-03 04:18:04 -0400

    adds validations to tournaments

--- hoop-around-backend/app/models/tournament.rb
+++ hoop-around-backend/app/models/tournament.rb
@@ -3,5 +3,13 @@ class Tournament < ApplicationRecord
     has_many :teams, through: :registrations 
      
 
+    validates_presence_of :name
+    validates_presence_of :venue 
+    validates_presence_of :date_time
+    validates_presence_of :host
     validates_presence_of :about
+    validates_presence_of :street
+    validates_presence_of :city
+    validates_presence_of :state
+    validates_presence_of :zipcode
 end
