2efdf75bf68cdb19c44bd52c2dfcb093deb5f8e9
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-03 03:44:42 -0400

    comments out application controller actions and methods

--- hoop-around-backend/app/controllers/application_controller.rb
+++ hoop-around-backend/app/controllers/application_controller.rb
@@ -1,32 +1,32 @@
 class ApplicationController < ActionController::API
-    helper_method(:current_user, :logged_in?, :redirect_if_not_reader)
-#I want current user, is_coach? is_player? 
-    def current_user
-        @current_user = Reader.find_by(id: session[:reader_id])
-    end
+#     helper_method(:current_user, :logged_in?, :redirect_if_not_reader)
+# #I want current user, is_coach? is_player? 
+#     def current_user
+#         @current_user = Reader.find_by(id: session[:reader_id])
+#     end
 
-    def logged_in?
-        current_user != nil
-    end
+#     def logged_in?
+#         current_user != nil
+#     end
 
-    def require_login
-        ((flash[:message] = "You must be logged in to do that") && (redirect_to '/login')) if !logged_in?
-    end
+#     def require_login
+#         ((flash[:message] = "You must be logged in to do that") && (redirect_to '/login')) if !logged_in?
+#     end
     
     
-    def check_reader(reader)
-        reader.id == current_user.id
-    end
+#     def check_reader(reader)
+#         reader.id == current_user.id
+#     end
   
-    def redirect_if_not_reader(reader)
-        if !check_reader(reader)
-            if params.to_s.include?("reviews")
-          flash[:message] = "Sorry, this is not your review!"
-            elsif params..to_s.include?("reviews")
-                flash[:message] = "Sorry, these are not your books!"
-            end
-          redirect_to "/home"
-        end
-    end
+#     def redirect_if_not_reader(reader)
+#         if !check_reader(reader)
+#             if params.to_s.include?("reviews")
+#           flash[:message] = "Sorry, this is not your review!"
+#             elsif params..to_s.include?("reviews")
+#                 flash[:message] = "Sorry, these are not your books!"
+#             end
+#           redirect_to "/home"
+#         end
+#     end
 
 end
