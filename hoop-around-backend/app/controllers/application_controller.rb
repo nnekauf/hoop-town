e83779116f0bbf6d90e69645a623671780be13c4
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-03 05:54:31 -0400

    sets up helper methods

--- hoop-around-backend/app/controllers/application_controller.rb
+++ hoop-around-backend/app/controllers/application_controller.rb
@@ -1,32 +1,45 @@
 class ApplicationController < ActionController::API
-#     helper_method(:current_user, :logged_in?, :redirect_if_not_user)
-# # #I want current user, is_coach? is_player? 
-#     def current_user
-#         @current_user = User.find_by(id: session[:user_id])
-#     end
+    helper_method(:current_user, :logged_in?, :coach_or_player, :only_coach)
+# #I want current user, is_coach? is_player? 
+    def current_user
+        @current_user = User.find_by(id: session[:user_id])
+    end
 
-#     def logged_in?
-#         current_user != nil
-#     end
+    def logged_in?
+        current_user != nil
+    end
 
-#     def require_login
-#         ((flash[:message] = "You must be logged in to do that") && (redirect_to '/login')) if !logged_in?
-#     end
+    def require_login
+        ((flash[:message] = "You must be logged in to do that") && (redirect_to '/login')) if !logged_in?
+    end
     
     
-#     def check_user(user)
-#         user.id == current_user.id
-#     end
+    def check_user(user)
+        user.id == current_user.id
+    end
   
-#     def redirect_if_not_user(user)
-#         if !check_user(user)
-#             if params.to_s.include?("reviews")
-#           flash[:message] = "Sorry, this is not your review!"
-#             elsif params..to_s.include?("reviews")
-#                 flash[:message] = "Sorry, these are not your books!"
-#             end
-#           redirect_to "/home"
-#         end
-#     end
+    def coach_or_player(user)
+        if user.role == "player"
+            return "player"
+        else
+            return "coach"
+        end
+    end
+
+    def only_coach(user)
+        if user.coach_or_player != "coach"
+            render json: {error: 'You must be a coach to do that'}
+        end
+
+    # def redirect_if_not_user(user)
+    #     if !check_user(user)
+    #         if params.to_s.include?("reviews")
+    #       flash[:message] = "Sorry, this is not your review!"
+    #         elsif params..to_s.include?("reviews")
+    #             flash[:message] = "Sorry, these are not your books!"
+    #         end
+    #       redirect_to "/home"
+    #     end
+    # end
 
 end
