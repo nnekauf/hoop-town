e53fa2861dfb00b98aab13da928be7300c16863e
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-03 04:04:42 -0400

    removes password from serializer

--- hoop-around-backend/app/serializers/user_serializer.rb
+++ hoop-around-backend/app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :username, :contact_number, :password_digest, :bio, :first_name, :last_name, :role
+  attributes :id, :email, :username, :contact_number, :bio, :first_name, :last_name, :role
 end
