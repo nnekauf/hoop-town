c422580a3c976a62d7a4f11920029252e74ee78a
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-03 03:02:43 -0400

    removes drop table pending migrations and removes belongs to in teams table

--- hoop-around-backend/db/migrate/20210601064514_create_teams.rb
+++ hoop-around-backend/db/migrate/20210601064514_create_teams.rb
@@ -2,7 +2,7 @@ class CreateTeams < ActiveRecord::Migration[6.1]
   def change
     create_table :teams do |t|
       t.string :name
-      t.belongs_to :coach, null: false, foreign_key: true
+     
 
       t.timestamps
     end
