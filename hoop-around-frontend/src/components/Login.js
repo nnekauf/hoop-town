ac013b989cd3d6bdded87559f911fb03f83bd551
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-04 03:33:00 -0400

    updates handleSubmit

--- hoop-around-frontend/src/components/Login.js
+++ hoop-around-frontend/src/components/Login.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import { updateLoginForm } from "../actions/loginForm.js"
 import { login } from "../actions/currentUser.js"
 
-const Login = ({loginFormData, updateLoginForm, login,}) => {
+const Login = ({loginFormData, updateLoginForm, login, history}) => {
 
     const handleInputChange = event => {
         const { name, value } = event.target
@@ -15,8 +15,10 @@ const Login = ({loginFormData, updateLoginForm, login,}) => {
       }
     
       const handleSubmit = event => {
-        
+        event.preventDefault()
+        login(loginFormData, history)
       }
+    
     
     return (
       <form onSubmit={handleSubmit}>
