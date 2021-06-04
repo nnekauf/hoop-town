35b8a30abb63520e6a78752a02333882f47521d4
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-04 02:43:42 -0400

    sets up Login component with a form for logging in

---
+++ hoop-around-frontend/src/components/Login.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Login = () => {
+
+   
+    return (
+      <form onSubmit={handleSubmit}>
+        <input placeholder="username"  name="username" type="text" onChange={handleInputChange} />
+        <input placeholder="password"  name="password" type="text" onChange={handleInputChange} />
+        <input type="submit" value="Log In"/>
+      </form>
+    )
+  }
+
+  
+export default Login
