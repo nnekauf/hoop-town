db750867ebaa3fcb411e5c57c52f907bc908cd95
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-04 02:35:43 -0400

    imports loginForm reducer into store

--- hoop-around-frontend/src/store.js
+++ hoop-around-frontend/src/store.js
@@ -2,6 +2,8 @@ import {createStore, combineReducers, compose, applyMiddleware} from 'redux'
 import listsReducer from './reducers/listsReducer'
 import usersReducer from './reducers/usersReducer'
 import currentUser from './reducers/currentUser'
+import loginForm from './reducers/loginForm'
+
 import {composeWithDevTools} from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 
@@ -11,7 +13,8 @@ import thunk from 'redux-thunk'
 const combinedReducer = combineReducers({
   lists: listsReducer, 
   users: usersReducer,
-  currentUser: currentUser
+  currentUser,
+  loginForm
 })
 
 const store = createStore(combinedReducer, compose(applyMiddleware(thunk), composeWithDevTools()))
