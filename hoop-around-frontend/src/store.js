62082b3192ffbd791e5d830301cbd69352393bea
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-04 01:53:17 -0400

    imports currentUser reducer into store

--- hoop-around-frontend/src/store.js
+++ hoop-around-frontend/src/store.js
@@ -1,6 +1,7 @@
 import {createStore, combineReducers, compose, applyMiddleware} from 'redux'
 import listsReducer from './reducers/listsReducer'
 import usersReducer from './reducers/usersReducer'
+import currentUser from './reducers/currentUser'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 
@@ -9,7 +10,8 @@ import thunk from 'redux-thunk'
 
 const combinedReducer = combineReducers({
   lists: listsReducer, 
-  users: usersReducer
+  users: usersReducer,
+  currentUser: currentUser
 })
 
 const store = createStore(combinedReducer, compose(applyMiddleware(thunk), composeWithDevTools()))
