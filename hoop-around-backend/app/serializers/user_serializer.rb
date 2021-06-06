e696473ebd5dde7c172628ffd88d11afc2752636
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-06 03:24:48 -0400

    adds belong to relationship and my_tournaments method to users serializer

--- hoop-around-backend/app/serializers/user_serializer.rb
+++ hoop-around-backend/app/serializers/user_serializer.rb
@@ -1,4 +1,11 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :username, :contact_number, :bio, :first_name, :last_name, :role, :team_id
- 
+
+  attributes :id, :email, :username, :contact_number, :bio, :first_name, :last_name, :role, :team_id, :my_tournaments
+ belongs_to :team
+  def my_tournaments
+   
+    current_user.team.tournaments.each {|t|
+  t}
+
+  end
 end
