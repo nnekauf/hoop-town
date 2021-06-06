594b6c053b2ae88494d2e9bfb2d9829977279557
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-06 16:15:27 -0400

    toggles home page buttons depending on login state of user

--- hoop-around-frontend/src/components/Home.js
+++ hoop-around-frontend/src/components/Home.js
@@ -1,16 +1,41 @@
 import React from 'react';
+import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 
-const Home = () => (
-  <div>
-    <span>
-      <Link to="/signup">Sign Up</Link> 
-      <br/>
-      <Link to="/login">Log In</Link>
-    </span>
-  </div>
+const Home = ({ state, loggedIn }) => {
+    
+    return (
+        <div className = "homePage">
+            { loggedIn ?
+                <>
+                    <Link className = "homeButton" to="/tournaments"> Find Tournaments</Link> 
+                        <br/>
+                    <Link className = "homeButton" to="/mygames"> My Games</Link>
+                    <br/>
+                    <Link className = "homeButton" to="/myprofile"> Profile</Link>
+                </> 
+                :
+                <>
+                    <Link className = "homeButton" to="/signup">Sign Up</Link> 
+                    <br/>
+                    <Link className = "homeButton" to="/login">Log In</Link>
+                </>
+            }
 
-);
+        </div>
+    )
 
-export default Home;
+
+};
+ 
+const mapStateToProps = state => {
+    return ({
+      state,
+      loggedIn: !!state.currentUser,
+      
+    })
+  }
+  
+  
+  export default connect(mapStateToProps)(Home)
