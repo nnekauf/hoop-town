6c7d3e3cd888d5c0f656132a92becc0613907f8f
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-06 06:02:31 -0400

    creates individual component for the tournament show page

---
+++ hoop-around-frontend/src/components/TournamentCard.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+
+const TournamentCard = tournament => {
+
+    
+
+  return (
+      <>
+      tournament ?
+      <>
+        <h3> {tournament.name}</h3>
+        <h3> {tournament.date_time}</h3>
+      </> :
+      <p> No Tournaments</p>
+      </>
+  )  
+
+}
+
+ 
+  export default TournamentCard
