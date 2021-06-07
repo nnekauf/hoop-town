7180a63d85d66c6da5705b60f3e9bd1a23efc068
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-07 19:07:23 -0400

    shows how many players are in a tournament

--- hoop-around-frontend/src/components/TournamentCard.js
+++ hoop-around-frontend/src/components/TournamentCard.js
@@ -11,6 +11,8 @@ const TournamentCard = ({tournament}) => {
         <h2> {tournament.name}</h2> <h3> {tournament.host}</h3>
         <h4> {tournament.date_time}</h4>
         <h4> {tournament.venue}</h4>| <h4>{tournament.city}</h4>, <h4>{tournament.state}</h4>
+        <br/>
+        <h4> # of teams | {tournament.teams.length}</h4>  
       </div>
   )  
 
