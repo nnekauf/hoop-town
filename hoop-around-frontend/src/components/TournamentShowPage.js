f030c0f448a600d849c2578f0a2ed9b288a58db6
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-07 19:15:21 -0400

    adds individual component for each tournaments show page

---
+++ hoop-around-frontend/src/components/TournamentShowPage.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+
+const TournamentShowPage = ({tournament}) => {
+
+    
+
+  return (
+      <div className = "tournamentShowPage">
+        <h2> {tournament.name}</h2> <h3> {tournament.host}</h3>
+        <h4> {tournament.date_time}</h4>
+        <h4> {tournament.venue}</h4>| <h4>{tournament.city}</h4>, <h4>{tournament.state}</h4>
+        <br/>
+        <h4> # of teams | {tournament.teams.length}</h4>  
+        <h4> {tournament.about}</h4>
+      </div>
+  )  
+
+}
+
+ 
+  export default TournamentShowPage
