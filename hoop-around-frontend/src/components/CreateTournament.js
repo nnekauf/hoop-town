607c392e40b14694e928e3b4c11dd362fad489c5
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-07 16:17:16 -0400

    removes comments and fixes formatting

--- hoop-around-frontend/src/components/CreateTournament.js
+++ hoop-around-frontend/src/components/CreateTournament.js
@@ -23,66 +23,19 @@ const CreateTournament = ({formData, updateTournamentForm, createTournament, his
 
     return (
         <form onSubmit={handleSubmit}>
-          <input
-            placeholder="name"
-            name="name"
-            onChange={handleChange}
-            value={formData.name}
-          /><br/>
-          <input
-            placeholder="venue"
-            name="venue"
-            onChange={handleChange}
-            value={formData.venue}
-          /><br/>
-          <input
-            placeholder="Date and time"
-            name="date"
-            onChange={handleChange}
-            value={formData.date}
-          /><br/>
-           <input
-            placeholder="host"
-            name="host"
-            onChange={handleChange}
-            value={formData.host}
-          /><br/>
-            <input
-            placeholder="about"
-            name="about"
-            onChange={handleChange}
-            value={formData.about}
-          /><br/>
-            <input
-            placeholder="street"
-            name="street"
-            onChange={handleChange}
-            value={formData.street}
-          /><br/>
-            <input
-            placeholder="city"
-            name="city"
-            onChange={handleChange}
-            value={formData.city}
-          /><br/>
-            <input
-            placeholder="state"
-            name="state"
-            onChange={handleChange}
-            value={formData.state}
-          /><br/>
-            <input
-            placeholder="zipcode"
-            name="zipcode"
-            onChange={handleChange}
-            value={formData.zipcode}
-          /><br/>
-          <input
-            type="submit"
-            value="Create Tournament" 
-          />
+          <input placeholder="name" name="name" onChange={handleChange} value={formData.name} />
+          <input placeholder="venue" name="venue" onChange={handleChange}   value={formData.venue}/>
+          <input placeholder= "Date and time" name="date" onChange={handleChange} value={formData.date}/>
+          <input placeholder="host" name="host"  onChange={handleChange}  value={formData.host} />
+          <input placeholder="about" name="about" onChange={handleChange}   value={formData.about} />
+          <input placeholder="street" name="street"   onChange={handleChange}  value={formData.street} />
+          <input placeholder="city" name="city"     onChange={handleChange}    value={formData.city} />
+          <input placeholder="state" name="state"    onChange={handleChange}   value={formData.state} />
+          <input placeholder="zipcode" name="zipcode"  onChange={handleChange} value={formData.zipcode} />
+          <input type="submit" value="Create Tournament"/> 
         </form>
-    )};
+    )
+};
 
   const mapStateToProps = state => {
     return {
@@ -91,16 +44,3 @@ const CreateTournament = ({formData, updateTournamentForm, createTournament, his
   }
   
   export default connect(mapStateToProps, {updateTournamentForm, createTournament})(CreateTournament)
-//   t.string "name"
-//   t.string "venue"
-//   t.datetime "date_time"
-//   t.string "host"
-//   t.text "about"
-//   t.datetime "created_at", precision: 6, null: false
-//   t.datetime "updated_at", precision: 6, null: false
-//   t.string "street"
-//   t.string "city"
-//   t.string "state"
-//   t.integer "zipcode"
-//   has_many :registrations
-//   has_many :teams, through: :registrations 
