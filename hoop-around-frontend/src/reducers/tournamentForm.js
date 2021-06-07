9aa606d2d3439e8b5a34024513e674193b27414e
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-07 15:49:02 -0400

    updats the cases removing the word new

--- hoop-around-frontend/src/reducers/tournamentForm.js
+++ hoop-around-frontend/src/reducers/tournamentForm.js
@@ -14,9 +14,9 @@ const initialState = {
  
   const tournamentForm = (state = initialState, action) => {
     switch (action.type) {
-      case "UPDATE_NEW_TOURNAMENT_FORM":
+      case "UPDATE_TOURNAMENT_FORM":
         return action.formData
-      case "RESET_NEW_TOURNAMENT_FORM":
+      case "RESET_TOURNAMENT_FORM":
         return initialState
       default:
         return state
