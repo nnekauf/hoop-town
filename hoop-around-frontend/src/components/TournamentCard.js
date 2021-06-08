4f860129a258729d86bca8ff3f4d7e193d66c2bf
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-07 21:21:22 -0400

    fixes the styling of the tournament cards

--- hoop-around-frontend/src/components/TournamentCard.js
+++ hoop-around-frontend/src/components/TournamentCard.js
@@ -8,11 +8,17 @@ const TournamentCard = ({tournament}) => {
 
   return (
       <div className = "tournamentCard">
-        <h2> {tournament.name}</h2> <h3> {tournament.host}</h3>
+        <div id="wrapper">
+          <h2> {tournament.name} | </h2>  <h3> {tournament.host}</h3>
+        </div>
         <h4> {tournament.date_time}</h4>
-        <h4> {tournament.venue}</h4>| <h4>{tournament.city}</h4>, <h4>{tournament.state}</h4>
-        <br/>
-        <h4> # of teams | {tournament.teams.length}</h4>  
+        <div id="wrapper">
+           <h4> {tournament.venue} | </h4> <h4>{tournament.city}</h4>, <h4>{tournament.state}</h4>
+        </div>
+        <div id="wrapper">
+          <h4> # of teams | {tournament.teams.length}</h4> 
+        </div>
+         
       </div>
   )  
 
