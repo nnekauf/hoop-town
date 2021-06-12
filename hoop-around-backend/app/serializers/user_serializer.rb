370cf670b05778681db27d135b1cf22363bd7cf9
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-12 19:14:50 -0400

    adds seeded data and fixes syntax error in serializer

--- hoop-around-backend/app/serializers/user_serializer.rb
+++ hoop-around-backend/app/serializers/user_serializer.rb
@@ -1,11 +1,12 @@
 class UserSerializer < ActiveModel::Serializer
 
   attributes :id, :email, :username, :contact_number, :bio, :first_name, :last_name, :role, :team_id, :my_tournaments
- belongs_to :team
-  def my_tournaments
-   
+  belongs_to :team
+  
+ def my_tournaments
+   if current_user &&current_user.team
     current_user.team.tournaments.each {|t|
   t}
-
+   end
   end
 end
