4a0ba75cd32d4d84f9512928f4aa0cfb753969c0
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-23 00:26:52 -0400

    adds note to format phone number in serialzer

--- hoop-around-backend/app/serializers/user_serializer.rb
+++ hoop-around-backend/app/serializers/user_serializer.rb
@@ -3,6 +3,7 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :username, :contact_number, :bio, :first_name, :last_name, :role, :team_id, :my_tournaments
   belongs_to :team
   
+  # add contact number serializer here to format number https://stackoverflow.com/questions/29323674/output-integer-as-phone-number-format-in-view
  def my_tournaments
   #  if current_user &&current_user.team
   #   current_user.team.tournaments.each {|t|
