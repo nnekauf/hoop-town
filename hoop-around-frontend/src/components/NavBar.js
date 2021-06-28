8c83045761aa9f76b3ca051f5904383348ad5d61
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-06-28 10:01:36 -0400

    removes uneeded imports and comments. Changes commeneted link from my games to saved games

--- hoop-around-frontend/src/components/NavBar.js
+++ hoop-around-frontend/src/components/NavBar.js
@@ -2,8 +2,6 @@ import React from 'react'
 import { connect } from 'react-redux'
 import {Link} from 'react-router-dom'
 import Logout from './Logout'
-import Login from './Login'
-import Signup from './Signup'
 
 const NavBar = ({ state, loggedIn }) => {
   
@@ -15,8 +13,7 @@ const NavBar = ({ state, loggedIn }) => {
         {state.currentUser.role} {state.currentUser.first_name}
         <Link className = "navLink" to="/">Home</Link>
         <Link className="navLink" to="/tournaments"> Find Tournaments </Link>
-        {/* <Link className="navLink" to="/mygames"> My Games </Link> */}
-        {/* <Link className="navLink" to="/myprofile"> Profile </Link> */}
+        {/* <Link className="navLink" to="/tournament/saved"> My Saved Games </Link> */}
           { state.currentUser.role === "coach" ? 
             <>
               <Link className="navLink" to="/tournaments/new"> Create Event </Link> 
