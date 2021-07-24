78adfc5f04fc6d78848007a65a59b8843f14f6ca
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-07-23 21:10:39 -0400

    seperates each input field into its own div

--- hoop-around-frontend/src/components/NewTournamentForm.js
+++ hoop-around-frontend/src/components/NewTournamentForm.js
@@ -24,20 +24,41 @@ const NewTournamentForm = ({formData, updateTournamentForm, createTournament, hi
     return (
       <>
         <form className= "newTournamentForm" onSubmit={handleSubmit}>
-          <input placeholder="name" name="name" onChange={handleChange} value={formData.name} />
-          <input placeholder="host" name="host"  onChange={handleChange}  value={formData.host} />
-          
-          <input placeholder="venue" name="venue" onChange={handleChange}   value={formData.venue}/>
-          <input placeholder="street" name="street"   onChange={handleChange}  value={formData.street} />
-          <input placeholder="city" name="city"     onChange={handleChange}    value={formData.city} />
-       
-       
-         <input placeholder= "Date and time" name="date" type="datetime-local" onChange={handleChange} value={formData.date}/>
-          <input placeholder="about" name="about"  onChange={handleChange}   value={formData.about} />
-          <input placeholder="state" name="state"    onChange={handleChange}   value={formData.state} />
-          <input placeholder="zipcode" name="zipcode" type="number" onChange={handleChange} value={formData.zipcode} />
-          
-          <input type="submit" value="Create Tournament"/> 
+          <div className = "tournamentDetails">
+            
+            <div className = "inputBox">
+              <input placeholder="name" name="name" onChange={handleChange} value={formData.name} />
+            </div>
+            <div className = "inputBox"> 
+              <input placeholder="host" name="host"  onChange={handleChange}  value={formData.host} />
+            </div>
+            <div className = "inputBox"> 
+              <input placeholder="venue" name="venue" onChange={handleChange}   value={formData.venue}/>
+            </div>
+            <div className = "inputBox"> 
+              <input placeholder="street" name="street"   onChange={handleChange}  value={formData.street} />
+            </div>
+            <div className = "inputBox"> 
+              <input placeholder="city" name="city"     onChange={handleChange}    value={formData.city} />            
+            </div>
+            <div className = "inputBox"> 
+              <input placeholder= "Date and time" name="date" type="datetime-local" onChange={handleChange} value={formData.date}/>        
+            </div>
+            <div className = "inputBox"> 
+             <input placeholder="about" name="about"  onChange={handleChange}   value={formData.about} />
+            </div>
+            <div className = "inputBox"> 
+              <input placeholder="state" name="state"    onChange={handleChange}   value={formData.state} />
+            </div>
+            <div className = "inputBox"> 
+              <input placeholder="zipcode" name="zipcode" type="number" onChange={handleChange} value={formData.zipcode} />
+            </div>
+        
+            <div className = "submitButton"> 
+                <input type="submit" value="Create Tournament"/>                         
+            </div>
+          </div>
+
         </form>
         </>
     )
