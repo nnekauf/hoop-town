5fbeaad516b421f3ebdee2ea4a5832f1cfb43418
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-07-24 01:24:01 -0400

    imports basketball videos and slider functionality

--- hoop-around-frontend/src/components/Home.js
+++ hoop-around-frontend/src/components/Home.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import AwesomeSlider from 'react-awesome-slider';
+import AwesomeSliderStyles from 'react-awesome-slider/src/styled/cube-animation.scss';
+import BasketballMatch from "../images/pexels-cottonbro-5275201.mp4"
+import Girl from "../images/pexels-cottonbro-6764723.mp4"
+import Guy from "../images/pexels-tima-miroshnichenko-5586534.mp4"
+import hoop from "../images/production ID_5192077.mp4"
 
 
 const Home = ({ state, loggedIn }) => {
-    
+
     return (
         <div className = "homePage">
             <h1 className="mainTitle"> Hoop Around</h1>
