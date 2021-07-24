6150dedb94a9a4463793d4177ae2389527947aae
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-07-24 00:58:03 -0400

    imports search filter to tournament index page

--- hoop-around-frontend/src/components/AllTournaments.js
+++ hoop-around-frontend/src/components/AllTournaments.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import TournamentCard from './TournamentCard'
+import SearchFilter from './SearchFilter'
 
 const AllTournaments = state => {
 
   return (
+    <>
+    <div className="searchBar">
+      {SearchFilter}
+    </div>
     
     <div className= "allTournaments">
     <br/>
-    
       { state.tournaments.length>0 ?
         <>
           {state.tournaments.map(t => (<TournamentCard tournament = {t} key = {t.id}/>))}
@@ -19,6 +23,7 @@ const AllTournaments = state => {
         </>
       }
     </div>
+    </>
   )
 }
 
