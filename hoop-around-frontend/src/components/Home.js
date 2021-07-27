62b78466fc389bd67cd029815a693aa440919c48
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-07-26 23:50:30 -0400

    adds react bootstrap npm package as well as importing it withing index.js

--- hoop-around-frontend/src/components/Home.js
+++ hoop-around-frontend/src/components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
-
+import bootstrap from 'bootstrap'
 import BasketballMatch from "../images/pexels-cottonbro-5275201.mp4"
 import Girl from "../images/pexels-cottonbro-6764723.mp4"
 import Guy from "../images/pexels-tima-miroshnichenko-5586534.mp4"
@@ -11,6 +11,7 @@ import hoop from "../images/production ID_5192077.mp4"
 const Home = ({ state, loggedIn }) => {
 
     return (
+        <>
         <div className = "homePage">
             <h1 className="mainTitle"> Hoop Around</h1>
             <h2 className= "title" > Street Ball <b>Reinvented</b> </h2>
@@ -31,6 +32,34 @@ const Home = ({ state, loggedIn }) => {
             }
 
         </div>
+{/* 
+        <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+            <div className="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img src= {Girl} className="d-block w-100" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                    <img src={Guy} className="d-block w-100" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                    <img src={hoop} className="d-block w-100" alt="..."/>
+                </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Previous</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Next</span>
+            </button>
+    </div> */}
+        </>
     )
 
 
--- hoop-around-frontend/src/index.js
+++ hoop-around-frontend/src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import {Provider} from 'react-redux'
 import store from './store.js'
 import {BrowserRouter as Router} from 'react-router-dom'
-
+import 'bootstrap/dist/css/bootstrap.min.css';  
 
 
 
