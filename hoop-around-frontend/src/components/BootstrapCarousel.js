dab1fdc51962dfc87754685c2af7c30c1c799b78
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-07-27 21:09:13 -0400

    uses react carousel instead of bootstrap carousel

--- hoop-around-frontend/src/components/BootstrapCarousel.js
+++ hoop-around-frontend/src/components/BootstrapCarousel.js
@@ -1,59 +1,27 @@
-import Carousel from 'react-bootstrap/Carousel'
+
 import React, { Component } from 'react'  
 import Players from "../images/carousel_2.mp4"
 import hoop from "../images/carousel_3.mp4"
 import team from "../images/carousel_1.mp4"
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 
 const BootstrapCarousel = () => {
   
     return (
-      <>
-        <Carousel>
-        <Carousel.Item>
-            <video
-            className="carousel"
-            type="video/mp4"
-            src={team}
-            loop autoPlay muted
-
-            alt="First slide"
-            />
-            <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-        <video
-            className="carousel"
-            type="video/mp4"
-            src={Players}
-            loop autoPlay muted
-            alt="Second slide"
-            />
-
-            <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-        <video
-            className="carousel"
-            type="video/mp4"
-            src={hoop}
-            loop autoPlay muted
-            alt="Third slide"
-            />
-
-            <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-            </Carousel.Caption>
-        </Carousel.Item>
-        
-        </Carousel>
-      </>
+      <div className="carousel-wrapper">
+            <Carousel>
+                <div>
+                    <img src={team} />
+                </div>
+                <div>
+                    <img src={Players} />
+                </div>
+                <div>
+                    <img src={hoop} />
+                </div>
+            </Carousel>
+        </div>
     )
     }
   
