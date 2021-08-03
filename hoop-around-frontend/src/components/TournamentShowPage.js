11b8536e0c3cdab2fcdc0364ce8f3fba52f4d2a2
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-08-03 01:13:44 -0400

    edits background colors for tournament show page

--- hoop-around-frontend/src/components/TournamentShowPage.js
+++ hoop-around-frontend/src/components/TournamentShowPage.js
@@ -8,19 +8,22 @@ const TournamentShowPage = ({tournament}) => {
   return (
       <div className="tournamentShowPage"> <br/> <br/>
         
-      
-        <h2> {tournament.name} TOURNAMENT</h2> <h3> HOSTED BY: {tournament.host}</h3>
-        <br/>
-        <h4>LOCATION: {tournament.venue} {tournament.city}, {tournament.state} {tournament.zipcode}</h4>
-        
-        <h4>TIME: {tournament.date_time}</h4>
-        
-        
-        <br/>
-         
-        <h4> ABOUT THIS TOURNAMENT</h4>
-        <p> {tournament.about}</p>
-        <h4> # of teams | {tournament.teams.length}</h4> 
+        <div className = "showPageCard">
+          <h2> {tournament.name} TOURNAMENT</h2> <h3> HOSTED BY: {tournament.host}</h3>
+          <br/>
+          <h4>LOCATION: {tournament.venue} {tournament.city}, {tournament.state} {tournament.zipcode}</h4>
+          
+          <h4>TIME: {tournament.date_time}</h4>
+          
+          
+          <br/>
+          
+          <h4> ABOUT THIS TOURNAMENT</h4>
+          <p> {tournament.about}</p>
+          <h4> # of teams | {tournament.teams.length}</h4> 
+
+        </div>
+
       </div>
   )  
 
