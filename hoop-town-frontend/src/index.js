b658e308124260f90d4c3a74fa6e7b948246870e
Nneka Ufumaka <ufumakanneka@gmail.com>
2021-09-12 22:00:51 -0500

    changes Browser router to Hash router

--- hoop-town-frontend/src/index.js
+++ hoop-town-frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {Provider} from 'react-redux'
 import store from './store.js'
-import {BrowserRouter as Router} from 'react-router-dom'
+import {HashRouter as Router} from 'react-router-dom'
 import'./fonts/UrbanJungle/Urbanjungledemo-ymAm.otf'
 
 
